0d1cc7c934180be7b44b5e24eca2e2c13d54b100
uptoser <450052550@qq.com>
2025-01-11 10:22:11 +0800

    first commit

---
+++ springcloud-netflix-consumer-product-feign-80/src/main/java/com/uptoser/springcloud/netflix/controller/ProductController_Feign.java
@@ -0,0 +1,38 @@
+package com.uptoser.springcloud.netflix.controller;
+
+import com.uptoser.springcloud.netflix.entities.Product;
+import com.uptoser.springcloud.netflix.service.ProductClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Auther: 梦学谷
+ */
+@RestController
+public class ProductController_Feign {
+
+   @Autowired
+   ProductClientService service;
+
+    @RequestMapping(value = "/consumer/product/add")
+    public boolean add(Product product) {
+        return service.add(product);
+    }
+
+    @RequestMapping(value = "/consumer/product/get/{id}")
+    public Product get(@PathVariable("id") Long id) {
+        return service.get(id);
+    }
+
+    @RequestMapping(value = "/consumer/product/list")
+    public List<Product> list() {
+        return service.list();
+    }
+
+
+
+}
---
+++ springcloud-netflix-provider-product-8001/src/main/java/com/uptoser/springcloud/netflix/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.uptoser.springcloud.netflix.service;
+
+
+
+import com.uptoser.springcloud.netflix.entities.Product;
+
+import java.util.List;
+
+/**
+ * @Auther: 梦学谷
+ */
+public interface ProductService {
+
+    boolean add(Product product);
+
+    Product get(Long id);
+
+    List<Product> list();
+
+}
