f28fe848a2a2332a8fd0094a36779fa0863277b6
uptoser <450052550@qq.com>
2025-01-11 13:01:55 +0800

    增加了feign的一些配置信息

--- springcloud-netflix-consumer-product-feign-80/src/main/java/com/uptoser/springcloud/netflix/ProductConsumer_80_Feign.java
+++ springcloud-netflix-consumer-product-feign-80/src/main/java/com/uptoser/springcloud/netflix/ProductConsumer_80_Feign.java
@@ -1,5 +1,7 @@
 package com.uptoser.springcloud.netflix;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -16,6 +18,9 @@ public class ProductConsumer_80_Feign {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductConsumer_80_Feign.class, args);
+        Logger logger = LoggerFactory.getLogger(ProductConsumer_80_Feign.class);
+        logger.info("http://localhost/consumer/product/get/1");
+        logger.info("http://localhost/consumer/product/list");
     }
 
 }
--- springcloud-netflix-consumer-product-feign-80/src/main/java/com/uptoser/springcloud/netflix/service/ProductClientService.java
+++ springcloud-netflix-consumer-product-feign-80/src/main/java/com/uptoser/springcloud/netflix/service/ProductClientService.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-/**
- * @Auther: 梦学谷
- */
 // fallback 作用，指定熔断处理类，如果被调用的方法处理异常，就会交给熔断处理类中的方法进行处理
 @FeignClient(value = "microservice-product", fallback = ProductClientServiceFallBack.class) //指定调用的微服务名称
 public interface ProductClientService {
